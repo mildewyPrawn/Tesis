b56443a662f0a8ca173127ac03382d9b6c2a7842
Emiliano <galeanaara@ciencias.unam.mx>
2023-07-01 14:47:11 -0600

    Refactor code
    add the isometric operations inside the Pts class and send the raw
    points to the class instead of play directly.

--- Tesis.py
+++ Tesis.py
@@ -37,7 +37,7 @@ def makeNeighborsInactive(board, x, y):
 def makeFullBoard():
     return [[True for i in range(DIM)] for j in range(DIM)]
 
-'''Función que toma las lineas leídas y regresa una lista de puntos en
+'''Funcion que toma las lineas leidas y regresa una lista de puntos en
 forma de tuplas'''
 def lines_to_tuples(pts):
     pts_as_tups = []
@@ -46,18 +46,15 @@ def lines_to_tuples(pts):
         pts_as_tups.append((float(tup[0]), float(tup[1])))
     return pts_as_tups
 
-'''Función que  crea el tablero  inicial. Tenemos que  usar isometrías
+'''Funcion que  crea el tablero  inicial. Tenemos que  usar isometrias
 para relocar los puntos, de otra  forma se ven muy juntos. Actualmente
 los voltea.'''
 def makeBoards(pts):
-    def f(t):
-        return ((t[0]*25)+250, (t[1]*25)+250)
-    board = list(map(lambda x: f(x), pts))
-    print(board)
+    board = pts.get_pts_iso()
     board_two = [(256, 258), (260, 257), (260, 255), (258, 254), (257, 256)]
     return (board, board_two)
 
-'''Función que dibuja los bordes. Estos bordes deberían servirnos para
+'''Funcion que dibuja los bordes. Estos bordes deberian servirnos para
 no ir al infinito.'''
 def drawBorder():
     stroke(0,255,0)
@@ -67,7 +64,7 @@ def drawBorder():
     line(695, 695, 5, 695);
     # p.hola()
 
-'''Función que muestra el tablero, dibuja bolitas en el mapa.'''
+'''Funcion que muestra el tablero, dibuja bolitas en el mapa.'''
 def showBoard(board):
     drawBorder() # TODO: crear dinamicamente el borde
     for pt in board:
--- Voronoi.pyde
+++ Voronoi.pyde
@@ -15,23 +15,22 @@ with open(file) as f:
 points = lines_to_tuples(lines)
 
 # TODO: crear un objeto
-pts = Pts(3)
-pts.hola()
+pts = Pts(points)
 
-'''Método que necesita processing  para inicializar todo, asignamos el
-tamaño  de la  ventana  y el  background. points  son  los puntos  que
+'''Metodo que necesita processing  para inicializar todo, asignamos el
+tamano  de la  ventana  y el  background. points  son  los puntos  que
 leemos.
 
 Hasta ahorita solo usamos b, b2 (este no tanto)'''
 def setup():
     global b, b2, points
     if MODE == "RANDOM":
-        b, b2 = makeBoards(points)
+        b, b2 = makeBoards(pts)
     size(700, 700)
     background(0)
     frameRate(FRAMERATE)
 
-'''Método para poder  presionar teclas, de momento solo  está la tecla
+'''Metodo para poder  presionar teclas, de momento solo  está la tecla
 de  reset que  será  para  rehacer todo  el  programa  con los  puntos
 dados.'''
 def keyPressed():
@@ -44,12 +43,12 @@ def keyPressed():
     elif key == "r":
         reset()
 
-'''Método reset,  servirá para volver  a empeazar le ejecución  con la
-configuración inicial.'''
+'''Metodo reset,  servira para volver  a empeazar le ejecucion  con la
+configuracion inicial.'''
 def reset():
     global THREAD, b, b2, bInert
     THREAD = "OFF"
-    b, b2 = makeBoards()
+    b, b2 = makeBoards(pts)
 
 '''Método para  dibujar, es  el main,  lo que  hace es  inicializar el
 board y posteriormente, va a ir actualizandolo con los movimientos.'''
