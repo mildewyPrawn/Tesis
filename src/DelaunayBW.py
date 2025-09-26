192be984f804fed61cac30e965eb5ef9b6d5fc2b
Emiliano Galeana <galeanaara@ciencias.unam.mx>
2025-09-25 21:18:48 -0600

    New Delaunay triangulation
    Use   the   Bowyer–Watson   algorithm   for   compute   the   delaunay
    triangulation, from  O(n³) to O(n²).  Maybe use  a DAG to  decreate to
    O(nlogn).

---
+++ src/DelaunayBW.py
@@ -0,0 +1,98 @@
+import math
+
+import Point as pt
+import Edge as ed
+import Triangle as tr
+import Circle as cr
+
+class DelaunayBW:
+
+    def __init__(self, pts):
+        if (len(pts) < 3):
+            raise Exception("The number of points needs to be at least three, you have: {}".format(len(pts)))
+        self.pts = pts
+        self.outer_triangle = self.outer_triangle(pts)
+        print('pts: {}'.format(self.pts))
+        print('OT: {}'.format(self.outer_triangle))
+        self.triangulate()
+
+    def get_triangulation(self):
+        return self.triangulation
+
+    def outer_triangle(self, pts):
+        minx = float('inf')
+        miny = float('inf')
+        maxx = float('-inf')
+        maxy = float('-inf')
+
+        for pti in pts:
+            minx = min(minx, pti.x)
+            miny = min(miny, pti.y)
+            maxx = max(maxx, pti.x)
+            maxy = max(maxy, pti.y)
+
+        dx = (maxx - minx) * 10
+        dy = (maxy - miny) * 10
+
+        p0 = pt.Point(minx - dx, miny - dy * 3)
+        p1 = pt.Point(minx - dx, maxy + dy)
+        p2 = pt.Point(maxx + dx * 3, maxy + dy)
+
+        return tr.Triangle(p0, p1, p2)
+
+    def triangulate(self):
+        triangles = [self.outer_triangle]
+
+        for pt in self.pts:
+            triangles = self.add_vertex(pt, triangles)
+
+            def __bounding_with_outer(self, triangle):
+                ot = self.outer_triangle
+                flag = not (triangle.a == ot.a or triangle.a == ot.b or triangle.a == ot.c or triangle.b == ot.a or triangle.b == ot.b or triangle.b == ot.c or triangle.c == ot.a or triangle.c == ot.b or triangle.c == ot.c)
+                return flag
+
+        triangles = list(filter(lambda x : __bounding_with_outer(self, x), triangles))
+
+        for tri in triangles:
+            circle = cr.Circle(a=tri.a, b=tri.b, c=tri.c)
+            circle.circumcircle()
+            x0y0, r = circle.origin, circle.radius
+            tri.create_circle(circle)
+
+        self.triangulation = triangles
+
+    def add_vertex(self, point, triangles):
+        def __filter_triangles(self):
+            new_triangles = []
+            edges = []
+            for tr in triangles:
+                if (tr.inCircumcircle(point)):
+                    edges.append(ed.Edge(tr.a, tr.b))
+                    edges.append(ed.Edge(tr.b, tr.c))
+                    edges.append(ed.Edge(tr.c, tr.a))
+                else:
+                    new_triangles.append(tr)
+
+            return (new_triangles, edges)
+
+        triangles, edges = __filter_triangles(self)
+        edges = self.unique_edges(edges)
+
+        for edg in edges:
+            triangles.append(tr.Triangle(edg.p1, edg.p2, point))
+
+        return triangles
+
+    def unique_edges(self, edges):
+        unique = []
+
+        for i in range(len(edges)):
+            is_unique = True
+            for j in range(len(edges)):
+                if (i != j and edges[i].is_equal(edges[j])):
+                    is_unique = False
+                    break
+            if is_unique:
+                unique.append(edges[i])
+
+        return unique
